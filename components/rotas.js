3cb10c1f8bef9edd0695d1c458365f46c0147e46
HeitorBalero <heitorbalero2@gmail.com>
2024-08-26 22:53:35 -0300

    primeiro commit

---
+++ components/rotas.js
@@ -0,0 +1,14 @@
+import { React } from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import { home } from "./home"
+import { cadastrar }from "./alterarCriptos"
+import { alterar }from "./alterarCriptos"
+
+const stack = createStackNavigator();
+
+export default function Rotas() {
+    return(
+
+    );
+}
